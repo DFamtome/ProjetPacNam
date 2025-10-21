1f5215ac955be18d37fa0d7a621ade0e8d13ae83
DFamtome <julien.jollivet@epita.fr>
2025-10-21 14:29:53 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo Installation de vscode et des extentions
+
+nix profile install nixpkgs#kdePackages.vscode
+nix profile install nixpkgs#vscode-extensions.ms-python.python
+
+echo Installation de pygame
+nix profile install nixpkgs#python311Packages.pygame
+
+nix-shell -p "python3.withPackages (ps: [ ps.pygame ])"
+
+rm ~/.cache/dmenu_run
