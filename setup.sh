ee3e7b08454298a7cd557d64518a89f16562db59
DFamtome <julien.jollivet@epita.fr>
2025-10-21 19:17:22 +0200

    Update setup.sh
    TEst

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 echo Installation de pygame et de vscode
-nix-shell -p python311Packages.pygame vscode vscode-extensions.ms-python.python
+nix-shell -p "python311Packages.pygame vscode vscode-extensions.ms-python.python (ps: [ ps.pygame ])"
 
 rm ~/.cache/dmenu_run
